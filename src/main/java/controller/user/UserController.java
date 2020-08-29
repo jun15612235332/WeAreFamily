1c81eae87129ad4ff1c6c69210fbc2627f42886c
jun15612235332 <sunxianjun0121@163.com>
2020-08-29 15:47:48 +0800

    调试配置

--- src/main/java/controller/user/UserController.java
+++ src/main/java/controller/user/UserController.java
@@ -2,14 +2,14 @@ package controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/main")
+    @RequestMapping("/auth")
     public String main() {
+        String str = "";
         return "main";
     }
 
